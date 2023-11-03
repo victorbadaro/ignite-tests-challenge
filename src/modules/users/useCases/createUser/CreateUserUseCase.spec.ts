507b3d245d95951972fc7d620747f9acb6d9bd1d
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-11-03 13:36:28 -0300

    test: add missing test for creating users

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,38 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe('Create User', () => {
+  beforeEach(()=> {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it('Should be able to create a new user', async () => {
+    const user = await createUserUseCase.execute({
+      name: 'User Test',
+      email: 'user.test@email.com',
+      password: '1234'
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a new user with the same email', () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: 'User Test',
+        email: 'user.test@email.com',
+        password: '1234'
+      });
+      await createUserUseCase.execute({
+        name: 'Second User Test',
+        email: 'user.test@email.com',
+        password: '12345'
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
