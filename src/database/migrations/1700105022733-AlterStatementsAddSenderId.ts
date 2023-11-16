071f020551bd3b2f6f1e5604dbca958cb83bc4f7
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-11-16 00:37:08 -0300

    feat: add a migration to add a sender_id column to the statements relation

---
+++ src/database/migrations/1700105022733-AlterStatementsAddSenderId.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm";
+
+export class AlterStatementsAddSenderId1700105022733 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.addColumn('statements', new TableColumn({
+        name: 'sender_id',
+        type: 'uuid'
+      }));
+
+      await queryRunner.createForeignKey('statements', new TableForeignKey({
+        name: 'statements_sender_id_fkey',
+        columnNames: ['sender_id'],
+        referencedTableName: 'users',
+        referencedColumnNames: ['id'],
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey('statements', 'statements_sender_id_fkey');
+      await queryRunner.dropColumn('statements', 'sender_id');
+    }
+
+}
